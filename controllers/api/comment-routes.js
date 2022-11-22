ba98079f2234be0fe7d11219db7f3909ac364898
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-11-22 18:08:41 +1100

    comment route, delete route added

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -69,3 +69,23 @@ router.put('/:id', withAuth, (req, res) => {
 });
 
 // DELETE route to delete comment using comment id 
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        },
+    })
+    .then ((commentData) => {
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment with this id'});
+            return;
+        }
+        res.json(commentData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
