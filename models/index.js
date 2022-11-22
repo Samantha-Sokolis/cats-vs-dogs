7258dd9d2a184d84f17df090c49dc57407d01f51
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-11-22 19:01:23 +1100

    updated model relationships

--- models/index.js
+++ models/index.js
@@ -1,1 +1,28 @@
 // relationships
+const User = require('./User');
+const Animals = require('./Animals');
+const Comment = require('./Comment');
+
+// User has many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+// Comment belong to user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+// Animals has many comment
+Animals.hasMany(Comment, {
+    foreignKey: 'animal_id',
+    onDelete: 'CASCADE'
+})
+
+// Comment belong to Animal
+Comment.belongsTo(Animals, {
+    foreignKey: 'animal_id',
+    onDelete: 'CASCADE'
+})
