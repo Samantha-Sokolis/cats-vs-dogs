43b17ce892a1c9348a87f908e682f5d5fa4fd54e
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-11-24 20:18:18 +1100

    home-routes updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,26 @@
+const { Category, Animals } = require('../models');
+
 const router = require('express').Router();
 
-// GET items for homepage
+// GET all categories for the homepage
 router.get('/', async (req, res) => {
   try {
+    const dbCategoryData = await Category.findAll({
+      include: [
+        {
+          model: Animals,
+          attributes: ['filename', 'description'],
+        },
+      ],
+    });
+
+    const categories = dbCategoryData.map((category) =>
+      gallery.get({ plain: true })
+    );
 
     // Send over the 'loggedIn' session variable to the 'homepage' template
-    res.render('homepage', {
+    res.render('homepage', { 
+      categories,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
@@ -13,6 +28,48 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// GET one category
+router.get('/category/:id', async (req, res) => {
+  try {
+    const dbCategoryData = await Category.findByPk(req.params.id, {
+      include: [
+        {
+          model: Animals,
+          attributes: [
+            'id',
+            'animal_breed',
+            'size',
+            'temperament',
+            'description',
+            'filename',
+          ],
+        },
+      ],
+    });
+
+    const category = dbCategoryData.get({ plain: true });
+    res.render('category', { category });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  } 
+});
+
+// GET one animals
+router.get('/animals/:id', async (req, res) => {
+  try {
+    const dbAnimalData = await Animals.findByPk(req.params.id);
+
+    const animal = dbAnimalData.get({ plain: true });
+
+    res.render('painting', { painting });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 
 // Login route
 router.get('/login', (req, res) => {
