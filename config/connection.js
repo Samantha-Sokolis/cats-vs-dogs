d0c6b057abc945d258ed4784d6e5e72969f70403
NT399 <nimeasht@gmail.com>
2022-11-27 18:40:50 +1100

    amending connection.js for heroku

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,29 @@ const cloudinary = require('cloudinary').v2;
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// Uncomment below if you would like to use LocalHost
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// Comment below if you wish to use localhost
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  },
+);
 
     cloudinary.config({ 
       cloud_name: 'dj99ibnyv', 
