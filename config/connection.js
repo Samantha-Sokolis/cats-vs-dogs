02ac505c445ad3f59a8ffa51725c72cdc562f9e5
NT399 <nimeasht@gmail.com>
2022-11-27 18:51:35 +1100

    changed config.js to correct connection

--- config/connection.js
+++ config/connection.js
@@ -16,16 +16,23 @@ const Sequelize = require('sequelize');
 //     });
 
 // Comment below if you wish to use localhost
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  },
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
+
 
     cloudinary.config({ 
       cloud_name: 'dj99ibnyv', 
